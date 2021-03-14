85b0d0ded18ea0002cfa3ed1a1afe3dda040f827
Eddie <eddie1998221@gmail.com>
2021-03-14 17:55:19 +0800

    1672. Richest Customer Wealth

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -5,23 +5,25 @@ using namespace std;
 
 class Solution {
 public:
-    int numIdenticalPairs(vector<int>& nums) {
-        int good_pair = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] == nums[j]) {
-                    good_pair++;
-                }
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].size(); j++) {
+                sum += accounts[i][j];
+            }
+            if (max < sum) {
+                max = sum;
             }
         }
-        return good_pair;
+        return max;
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> nums = { 1,2,3,1,1,3 };
-    cout << s.numIdenticalPairs(nums);
+    vector<vector<int>> nums = { {1, 2, 3}, {3, 2, 1} };
+    cout << s.maximumWealth(nums);
 }
 
--- README.md
+++ README.md
@@ -48,6 +48,7 @@ Leetcode
 1470. [Shuffle the Array](https://leetcode.com/problems/shuffle-the-array/ "Shuffle the Array")
 1480. [Running Sum of 1d Array](https://leetcode.com/problems/running-sum-of-1d-array/ "Running Sum of 1d Array")
 1512. [Number of Good Pairs](https://leetcode.com/problems/number-of-good-pairs/ "Number of Good Pairs")
+1672. [Richest Customer Wealth](https://leetcode.com/problems/richest-customer-wealth/ "Richest Customer Wealth")
 
 ### Median  
 ------
