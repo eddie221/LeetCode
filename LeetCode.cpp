149d37f5fb217a7490c195445bcd5d0db1a31d20
Eddie <eddie1998221@gmail.com>
2021-03-14 16:31:24 +0800

    9. Palindrome Number

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -4,25 +4,28 @@ using namespace std;
 
 class Solution {
 public:
-    int reverse(int x) {
-        int res = -1;
-        int r_num = 0;
-
-        while (x != 0) {
-            res = x % 10;
-            x = x / 10;
-            if (r_num > INT_MAX / 10 || (r_num == INT_MAX / 10 && res >= 7)) return 0;
-            if (r_num < INT_MIN / 10 || (r_num == INT_MIN / 10 && res < -8)) return 0;
-            r_num = r_num * 10 + res;
+    long long reverse(int x) {
+        long long rev = 0;
+        while (x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
         }
+        return rev;
+    }
 
-        return r_num;
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        else {
+            return x == reverse(x);
+        }
     }
 };
 
 int main()
 {
     Solution s;
-    cout << s.reverse(123);
+    cout << s.reverse(121);
 }
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Leetcode
 ===========================
 ## Statistics
-* [Easy(1)](#easy)
+* [Easy(3)](#easy)
 * [Median(1)](#median)
 * [Hard(1)](#hard)
 
@@ -9,6 +9,7 @@ Leetcode
 ------
 2. [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/ "Add Two Numbers")
 7. [Reverse Integer](https://leetcode.com/problems/reverse-integer/ "Reverse Integer")
+9. [Palindrome Number](https://leetcode.com/problems/palindrome-number/ "Palindrome Number")
 ### Median  
 ------
 3. [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/ "Longest Substring Without Repeating Characters")
