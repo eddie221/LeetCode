bd9cf79f1c6eba22f20b548c4e4a8bad914c531b
Eddie <eddie1998221@gmail.com>
2021-03-31 12:25:51 +0800

    1365. How Many Numbers Are Smaller Than the Current Number

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,80 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//class Solution {
-//public:
-//    int minPartitions(string n) {
-//        char max = '0';
-//        for (int i = 0; i < n.length(); i++) {
-//            if (n[i] > max) {
-//                max = n[i];
-//            }
-//        }
-//        return max - 48;
-//    }
-//};
-
-class ParkingSystem {
+class Solution {
 public:
-    ParkingSystem(int big, int medium, int small):big(big), medium(medium), small(small) {
-        
-    }
-
-    bool addCar(int carType) {
-        switch (carType) {
-        case 1:
-            if (big > 0) {
-                big--;
-                return true;
-            }
-            else {
-                return false;
-            }
-            break;
-        case 2:
-            if (medium > 0) {
-                medium--;
-                return true;
-            }
-            else {
-                return false;
-            }
-            break;
-        case 3:
-            if (small > 0) {
-                small--;
-                return true;
-            }
-            else {
-                return false;
-            }
-            break;
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> sorted_nums = nums;
+        vector<int> result;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            result.push_back(find(sorted_nums.begin(), sorted_nums.end(), nums[i]) - sorted_nums.begin());
         }
-        return false;
+        return result;
     }
-private:
-    int big = 0, medium = 0, small = 0;
 };
 
 int main()
 {
-    ParkingSystem* parkingSystem = new ParkingSystem(1, 1, 0);
-    parkingSystem->addCar(1); // return true because there is 1 available slot for a big car
-    parkingSystem->addCar(2); // return true because there is 1 available slot for a medium car
-    parkingSystem->addCar(3); // return false because there is no available slot for a small car
-    parkingSystem->addCar(1); // return false because there is no available slot for a big car. It is already occupied.
-    /*Solution s;
-    vector<string> 
-    cout << s.minPartitions("27346209830709182346");*/
+    Solution s;
+    //s.smallerNumbersThanCurrent("27346209830709182346");
     /*while (r != nullptr) {
         cout << r->val << endl;
         r = r->next;
     }*/
-    /*vector<double> result;
-    result = s.averageOfLevels(five);
+    vector<int> result;
+    vector<int> nums = { 8,1,2,2,3 };
+    result = s.smallerNumbersThanCurrent(nums);
     for (double r : result) {
         cout << r << endl;
-    }*/
+    }
 }
 
