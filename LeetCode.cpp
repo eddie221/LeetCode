93c2ae1a7be55f219fdb8cecc7449ead5e5a2ac0
Eddie <eddie1998221@gmail.com>
2021-04-05 19:38:25 +0800

    101. Symmetric Tree

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,27 +1,54 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    bool checkIfExist(vector<int>& arr) {
-
-        for (int i = 0; i < arr.size(); i++) {
-            if (find(arr.begin(), arr.end(), arr[i] * 2) != arr.end() && find(arr.begin(), arr.end(), arr[i] * 2) - arr.begin() != i) {
-                return true;
-            }
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
         }
-        return false;
+        else {
+            return isSymemtric(root->left, root->right);
+        }
+    }
+    
+    bool isSymemtric(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr) {
+            return true;
+        }
+        else if ((left == nullptr && right != nullptr) || (left != nullptr && right == nullptr) || (left->val != right->val)) {
+            return false;
+        }
+        else {
+            return isSymemtric(left->left, right->right) && isSymemtric(left->right, right->left);
+        }
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> arr = { -2,0,10,-19,4,6,-8 };
-    cout << s.checkIfExist(arr);
+    
+    TreeNode* three1 = new TreeNode(3);
+    TreeNode* three2 = new TreeNode(3);
+    TreeNode* four1 = new TreeNode(4);
+    TreeNode* four2 = new TreeNode(4);
+    TreeNode* two1 = new TreeNode(2, three1, four1);
+    TreeNode* two2 = new TreeNode(2, four2, three2);
+    TreeNode* one = new TreeNode(1, two1, two2);
+    cout << s.isSymmetric(one);
     /*while (r != nullptr) {
         cout << r->val << endl;
         r = r->next;
