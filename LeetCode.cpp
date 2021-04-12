b8ac09a634184fca8b4cbf9dac523dcd7c5473df
Eddie <eddie1998221@gmail.com>
2021-04-12 20:49:00 +0800

    1018. Binary Prefix Divisible By 5

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,70 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <utility>
 
 using namespace std;
 
 class Solution {
 public:
-    bool isLongPressedName(string name, string typed) {
-        int i = 1;
-        int j = 1;
-        for (; i < name.size();i++, j++) {
-            if (name[i - 1] == typed[j - 1]) {
-                int name_c = 1;
-                int typed_c = 1;
-                while (name[i] == name[i - 1] && i < name.size()) {
-                    name_c++;
-                    i++;
-                }
-                while (typed[j] == typed[j - 1] && j < typed.size()) {
-                    typed_c++;
-                    j++;
-                }
-                if (name_c > typed_c) {
-                    return false;
-                }
+    vector<bool> prefixesDivBy5(vector<int>& A) {
+        vector<bool> result;
+        int val = 0;
+        for (int i = 0; i < A.size(); i++) {
+            val = val * 2 + A[i];
+            if (val % 5 == 0) {
+                result.push_back(true);
             }
             else {
-                return false;
+                result.push_back(false);
             }
+            val = val % 5;
+
         }
-        
-        if (name[i - 1] != typed[j - 1]) {
-            return false;
-        }
-        if (i < name.size() || j < typed.size()) {
-            int name_c = 1;
-            int typed_c = 1;
-            while (i < name.size() && name[i] == name[i - 1]) {
-                name_c++;
-                i++;
-            }
-            while (j < typed.size() && typed[j] == typed[j - 1]) {
-                typed_c++;
-                j++;
-            }
-            if (name_c > typed_c || j != typed.size()) {
-                return false;
-            }
-        }
-        return true;
+
+        return result;
     }
 };
 
 int main()
 {
     Solution s;
-    cout << s.isLongPressedName("vtkgn", "vttkgnn");
+    vector<int> A = { 1,1,1 };
+    //cout << s.prefixesDivBy5(A);
     /*while (r != nullptr) {
         cout << r->val << endl;
         r = r->next;
     }*/
-    /*vector<int> result;
+    vector<bool> result;
     vector<int> nums = { 8,1,2,2,3 };
-    result = s.smallerNumbersThanCurrent(nums);
+    result = s.prefixesDivBy5(nums);
     for (double r : result) {
         cout << r << endl;
-    }*/
+    }
 }
 
