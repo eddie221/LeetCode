3ea7a90a06438dea31956fe058fe3e3d4ca8f139
Eddie <eddie1998221@gmail.com>
2021-04-29 15:10:41 +0800

    160. Intersection of Two Linked Lists

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -4,33 +4,52 @@
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        int count = 1;
-        int major = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            if (count == 0) {
-                major = nums[i];
-            }
-            if (major != nums[i]) {
-                count -= 1;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* A = headA, * B = headB;
+        while (A != B) {
+            if (A == nullptr) {
+                A = headB;
             }
             else {
-                count += 1;
+                A = A->next;
             }
-            
+            if (B == nullptr) {
+                B = headA;
+            }
+            else {
+                B = B->next;
+            }
         }
-        return major;
+        return A;
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> nums = { 2,2,1,1,1,2,2 };
-    cout << s.majorityElement(nums);
+    ListNode* intersection2 = new ListNode(4);
+    ListNode* intersection1 = new ListNode(8);
+    intersection1->next = intersection2;
+    ListNode* A_1 = new ListNode(4);
+    ListNode* A_2 = new ListNode(1);
+    A_1->next = A_2;
+    //A_2->next = intersection1;
+
+    ListNode* B_1 = new ListNode(5);
+    ListNode* B_2 = new ListNode(6);
+    ListNode* B_3 = new ListNode(1);
+    B_1->next = B_2;
+    B_2->next = B_3;
+    //B_3->next = intersection1;
+    cout << s.getIntersectionNode(A_1, B_1)->val;
 
     /* vector<bool> result;
      vector<int> nums = { 8,1,2,2,3 };
