7b1e19cefdfd6fd199bd6dbc9e6650ac81be2497
Eddie <eddie1998221@gmail.com>
2021-06-05 14:23:27 +0800

    1037. Valid Boomerang

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,31 +6,31 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] != s[s.length() - i - 1]) {
+    bool isBoomerang(vector<vector<int>>& points) {
+        if (points[0] == points[1] || points[1] == points[2] || points[0] == points[2]) {
+            return false;
+        }
+        else {
+            if (points[0][0] == points[1][0] && points[0][0] == points[2][0] && points[1][0] == points[2][0]) {
                 return false;
             }
-        }
-        return true;
-    }
-    bool validPalindrome(string s) {
-        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-            if (s[i] != s[j]) {
-                if (isPalindrome(s.substr(i, j - i)) || isPalindrome(s.substr(i + 1, j - i))) {
-                    return true;
-                }
+            else if(points[0][1] == points[1][1] && points[0][1] == points[2][1] && points[1][1] == points[2][1]) {
                 return false;
             }
+            else if (float(points[0][0] - points[1][0]) / float(points[0][1] - points[1][1]) != float(points[1][0] - points[2][0]) / float(points[1][1] - points[2][1])) {
+                return true;
+            }
         }
-        return true;
+        
+        return false;
     }
 };
 
 int main()
 {
     Solution s;
-    cout << s.validPalindrome("abc");
+    vector<vector<int>> points = { {1, 1}, {2, 3}, {3, 2} };
+    cout << s.isBoomerang(points);
     /* vector<bool> result;
      vector<int> nums = { 8,1,2,2,3 };
      result = s.intToRoman(nums);
