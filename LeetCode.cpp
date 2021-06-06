19635fadf0942912b7ab485b7e63a5cd21630ad1
Eddie <eddie1998221@gmail.com>
2021-06-06 17:19:59 +0800

    111. Minimum Depth of Binary Tree

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -3,28 +3,50 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),right(right) {}
+};
+
 class Solution {
 public:
-    int maxRepeating(string sequence, string word) {
-        int count = 0, max_count = 0;
-        for (int i = 0, j = 0; i < sequence.length(); i++) {
-            count = 0;
-            while (i + word.length() * count < sequence.length() && sequence.substr(i + word.length() * count, word.length()) == word) {
-                count++;
-            }
-            //i = i + word.length() * count;
-            if (max_count < count) {
-                max_count = count;
-            }
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
         }
-        return max_count;
+        if (root->left == nullptr && root->right == nullptr) {
+            return 1;
+        }
+        else {
+            int left = INT_MAX, right = INT_MAX;
+            if (root->left != nullptr) {
+                left = minDepth(root->left) + 1;
+            }
+            if (root->right != nullptr) {
+                right = minDepth(root->right) + 1;
+            }
+            cout << "left : " << left << " right : " << right << endl;
+            return min(left, right);
+        }
+        return 0;
     }
 };
 
 int main()
 {
     Solution s;
-    cout << s.maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba");
+    TreeNode* head = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+    cout << s.minDepth(head) << endl;
+    TreeNode* head2 = nullptr;
+    cout << s.minDepth(head2) << endl;
+    TreeNode* head3 = new TreeNode(3, new TreeNode(9, new TreeNode(20, new TreeNode(15, new TreeNode(7), nullptr), nullptr), nullptr), nullptr);
+    cout << s.minDepth(head3) << endl;
+    TreeNode* head4 = new TreeNode(3, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+    cout << s.minDepth(head4) << endl;
     /* vector<bool> result;
      vector<int> nums = { 8,1,2,2,3 };
      result = s.intToRoman(nums);
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Leetcode
 ===========================
 ## Statistics
-* [Easy(105)](#easy)
+* [Easy(106)](#easy)
 * [Median(14)](#median)
 * [Hard(1)](#hard)
 
@@ -112,6 +112,7 @@ Leetcode
 103. [Number of Segments in a String](https://leetcode.com/problems/valid-boomerang/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/95904bc21d53742c2b3be57162efed7d3e5b63cf/LeetCode.cpp "Solution Link")
 104. [First Bad Version](https://leetcode.com/problems/first-bad-version/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/2db35cab68ffd88bbf184f95d1ce817d5e4a4f27/LeetCode.cpp "Solution Link")
 105. [Word Pattern](https://leetcode.com/problems/word-pattern/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/bfdb6c49489719ed104a80f8b0d2cacb2a5b34ef/LeetCode.cpp "Solution Link")
+106. [Maximum Repeating Substring](https://leetcode.com/problems/maximum-repeating-substring/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/827ddb72d43232596d1fbc0b8a0f91d00211ddeb/LeetCode.cpp "Solution Link")
 
 ### Median  
 ------
