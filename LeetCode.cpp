18628830d412116beee14ab38a0b4780c8e2aa7f
Eddie <eddie1998221@gmail.com>
2021-06-14 15:31:16 +0800

    1033. Moving Stones Until Consecutive

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -7,59 +7,42 @@ using namespace std;
 
 class Solution {
 public:
-    string reorderSpaces(string text) {
-        int count = 0;
-        vector<string> str_list;
-        string tmp = "";
-        for (int i = 0; i < text.length(); i++) {
-            if (text[i] == ' ') {
-                count++;
-                if (tmp != "") {
-                    str_list.push_back(tmp);
-                }
-                tmp = "";
-            }
-            else {
-                tmp = tmp + text[i];
-            }
+    vector<int> numMovesStones(int a, int b, int c) {
+        if (a > b) {
+            b = a ^ b;
+            a = a ^ b;
+            b = a ^ b;
         }
-        if (tmp != "") {
-            str_list.push_back(tmp);
+        if (b > c) {
+            b = c ^ b;
+            c = c ^ b;
+            b = c ^ b;
         }
-        int space, left;
-        if (str_list.size() != 1) {
-            space = count / (str_list.size() - 1);
-            left = count % (str_list.size() - 1);
+        if (a > b) {
+            b = a ^ b;
+            a = a ^ b;
+            b = a ^ b;
         }
-        else {
-            space = 0;
-            left = count;
+        if (a + 1 == b && b + 1 == c) {
+            return { 0, 0 };
         }
-        string result = "";
-        for (int i = 0; i < str_list.size() - 1; i++) {
-            result = result + str_list[i];
-            cout << str_list[i] << endl;
-            for (int j = 0; j < space; j++) {
-                result = result + ' ';
-            }
+        else if (a + 1 == b || b + 1 == c || a + 2 == b || b + 2 == c) {
+            return { 1, b - a - 1 + c - b - 1 };
         }
-        result = result + str_list.back();
-        for (int i = 0; i < left; i++) {
-            result = result + ' ';
-        }
-        return result;
+        return { 2, b - a - 1 + c - b - 1 };
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> nums = { 3,6,1,0 };
-    cout << s.reorderSpaces("  this   is  a sentence ") << endl;
-    cout << s.reorderSpaces(" practice   makes   perfect") << endl;
-    cout << s.reorderSpaces("hello   world") << endl;
-    cout << s.reorderSpaces("  walks  udp package   into  bar a") << endl;
-    cout << s.reorderSpaces("a") << endl;
+    vector<int> ans;
+    ans = s.numMovesStones(1, 2, 5);
+    cout << ans[0] << " : " << ans[1] << endl;
+    ans = s.numMovesStones(4,3,2);
+    cout << ans[0] << " : " << ans[1] << endl;
+    ans = s.numMovesStones(3, 5,1 );
+    cout << ans[0] << " : " << ans[1] << endl;
     /* vector<bool> result;
      vector<int> nums = { 8,1,2,2,3 };
      result = s.intToRoman(nums);
