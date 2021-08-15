1b9d8d24a714245c5be10f6108842ff0d0f63bca
Eddie <eddie1998221@gmail.com>
2021-08-15 16:07:02 +0800

    338. Counting Bits

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -8,14 +8,23 @@ using namespace std;
 
 class Solution {
 public:
-    bool canWinNim(int n) {
-        if (n <= 3) {
-            return true;
+    vector<int> countBits(int n) {
+        vector<int> r;
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                r.push_back(0);
+            }
+            else if(i == 1 || i == 2) {
+                r.push_back(1);
+            }
+            else if(i % 2 == 1){
+                r.push_back(r[i >> 1] + 1);
+            }
+            else {
+                r.push_back(r[i >> 1]);
+            }
         }
-        if (n % 4 == 0) {
-            return false;
-        }
-        return true;
+        return r;
     }
 };
 
@@ -23,6 +32,9 @@ int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     Solution s;
-    cout << s.canWinNim(15) << endl;;
+    vector<int> r = s.countBits(10);
+    for (auto x : r) {
+        cout << x << endl;
+    }
     return 0;
 }
--- README.md
+++ README.md
@@ -160,6 +160,7 @@ Leetcode
 151. [Minimum Moves to Equal Array Elements](https://leetcode.com/problems/minimum-moves-to-equal-array-elements/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/e29893f92387235e80f7dd24b5ad594ac2c2922f/LeetCode.cpp "Solution Link")
 152. [Binary Tree Paths](https://leetcode.com/problems/binary-tree-paths/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/51c46ed3d258e7d557b140e1762626046e66ed80/LeetCode.cpp "Solution Link")
 153. [Add Digits](https://leetcode.com/problems/add-digits/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/556dd4024d8006e1fcb307ad5d796864f058a7a2/LeetCode.cpp "Solution Link")
+154. [Nim Game](https://leetcode.com/problems/nim-game/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/b608ecf3cb7bfcc2dd15f0337e9a14e8f7ae4a50/LeetCode.cpp "Solution Link")
 
 ### Median  
 ------
