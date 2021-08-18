23ef30875a30c42234a6f6a37c30a922b9b7940b
Eddie <eddie1998221@gmail.com>
2021-08-18 16:35:54 +0800

    404. Sum of Left Leaves

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <stack>
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int i = 0;
-        for (int j = 0; i < s.length() && j < t.length();) {
-            while (i < s.length() && j < t.length() && s[i] != t[j]) {
-                j++;
+    int sumOfLeftLeaves(TreeNode* root) {
+        stack<TreeNode*> container;
+        container.push(root);
+        TreeNode* recent = root;
+        int sum = 0;
+        while (!container.empty()) {
+            recent = container.top();
+            container.pop();
+            if (recent->left != nullptr) {
+                if (recent->left->left == nullptr && recent->left->right == nullptr) {
+                    sum += recent->left->val;
+                }
+                container.push(recent->left);
             }
-            if (s[i] == t[j]) {
-                i++;
-                j++;
+            if (recent->right != nullptr) {
+                container.push(recent->right);
             }
         }
-        if (i == s.length()) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return sum;
     }
 };
 
@@ -30,7 +41,7 @@ int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     Solution s;
-    cout << s.isSubsequence("abc", "ahbgdc");
-    cout << s.isSubsequence("a", "c");
+    TreeNode* root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+    cout << s.sumOfLeftLeaves(root);
     return 0;
 }
