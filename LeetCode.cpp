725b400529611272a746a82329187dc0d760188f
Eddie <eddie1998221@gmail.com>
2021-08-18 16:59:20 +0800

    409. Longest Palindrome

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <map>
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
 public:
-    int sumOfLeftLeaves(TreeNode* root) {
-        stack<TreeNode*> container;
-        container.push(root);
-        TreeNode* recent = root;
-        int sum = 0;
-        while (!container.empty()) {
-            recent = container.top();
-            container.pop();
-            if (recent->left != nullptr) {
-                if (recent->left->left == nullptr && recent->left->right == nullptr) {
-                    sum += recent->left->val;
-                }
-                container.push(recent->left);
-            }
-            if (recent->right != nullptr) {
-                container.push(recent->right);
+    int longestPalindrome(string s) {
+        map<char, int> count;
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i]]++;
+        }
+        int length = 0;
+        bool mid = false;
+        for (map<char, int>::iterator it = count.begin(); it != count.end(); it++) {
+            length += it->second - it->second % 2;
+            if (!mid && it->second % 2 == 1) {
+                mid = true;
+                length += it->second % 2;
             }
         }
-        return sum;
+        return length;
     }
 };
 
@@ -41,7 +28,6 @@ int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     Solution s;
-    TreeNode* root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-    cout << s.sumOfLeftLeaves(root);
+    cout << s.longestPalindrome("abccccdd");
     return 0;
 }
