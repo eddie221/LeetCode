7a312e86dc22422dae9cdbb5c454a59377c67b4f
Eddie <eddie1998221@gmail.com>
2021-10-01 22:43:13 +0800

    230. Kth Smallest Element in a BST

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,42 +1,36 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        if (n == 0) {
-            return {};
-        }
-        else if (n == 1) {
-            return { "()" };
-        }
-        else {
-            for (int i = 0; i < n; i++) {
-                vector<string> left = generateParenthesis(i);
-                vector<string> right = generateParenthesis(n - i - 1);
-                if (left.size() == 0) {
-                    for (int j = 0; j < right.size(); j++) {
-                        result.push_back("()" + right[j]);
-                    }
-                }
-                else if (right.size() == 0) {
-                    for (int j = 0; j < left.size(); j++) {
-                        result.push_back("(" + left[j] + ")");
-                    }
-                }
-                else {
-                    for (int j = 0; j < left.size(); j++) {
-                        for (int k = 0; k < right.size(); k++) {
-                            result.push_back("(" + left[j] + ")" + right[k]);
-                        }
-                    }
-                }
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> container;
+        container.push(root);
+        while (!container.empty()) {
+            while (root != nullptr) {
+                container.push(root);
+                root = root->left;
             }
+            root = container.top();
+            k--;
+            container.pop();
+            if (k == 0) {
+                return root->val;
+            }
+            root = root->right;
         }
-        return result;
+        return 0;
     }
 };
 
@@ -44,10 +38,8 @@ int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     Solution s;
-    vector<string> result = s.generateParenthesis(3);
-    cout << "ans : " << endl;
-    for (auto x : result) {
-        cout << x << endl;
-    }
+    //TreeNode* root = new TreeNode(3, new TreeNode(1, nullptr, new TreeNode(2)), new TreeNode(4));
+    TreeNode* root = new TreeNode(5, new TreeNode(3, new TreeNode(2, new TreeNode(1), nullptr), new TreeNode(4)), new TreeNode(6));
+    cout << s.kthSmallest(root, 3);
     return 0;
 }
--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Leetcode
 ===========================
 ## Statistics
 * [Easy(168)](#easy)
-* [Median(27)](#median)
+* [Median(28)](#median)
 * [Hard(1)](#hard)
 
 ### Easy
@@ -205,6 +205,7 @@ Leetcode
 25. [Subsets](https://leetcode.com/problems/subsets/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/7ac722b54e8d312433c55f2c330d5ba5744c10d2/LeetCode.cpp "Solution Link")
 26. [Rotate Image](https://leetcode.com/problems/rotate-image/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/766bda8eca79b2d19181aa405b75b91b005cdf52/LeetCode.cpp "Solution Link")
 27. [Group the People Given the Group Size They Belong To](https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/6ae517d89142a087bfffe4a8ac70774cc17db4c9/LeetCode.cpp "Solution Link")
+28. [Generate Parentheses](https://leetcode.com/problems/generate-parentheses/ "Problem Link") [(My sol.)](https://github.com/eddie221/LeetCode/blob/cc22a561092b8f1b3e37cc86996c2a0099df7c68/LeetCode.cpp "Solution Link")
 
 ### Hard  
 ------
